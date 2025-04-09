d252c0bf4f81385b1f52cc3e6ddcbc12e94e2000
sayali sakharkar <sayalisakharkar001@gmail.com>
2025-04-10 00:36:17 +0530

    this is modified code added few functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-
+import Player from './components/Player.jsx';
 function App() {
-  
-
-  return <p>soon...</p>;
+  return( 
+  <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player initialName="Player 1" symbol= "X"/>
+        <Player initialName="Player 2" symbol= "O"/>
+      </ol>
+      GAME BOARD
+    </div>
+    Log
+  </main>
+  );
 
     
   
